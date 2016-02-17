84c38e19e9d2936aefd07b017f4ad426d48ad781
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-02-17 22:21:51 +0100

    add subject to authorization token

--- src/server/actions/authentications/createToken.js
+++ src/server/actions/authentications/createToken.js
@@ -2,7 +2,10 @@ import jwt from 'jsonwebtoken';
 
 export default function (globals) {
   return function createToken(params, callback) {
-    var token = jwt.sign({ id: params.id }, globals.config.jwtToken.secret, { expiresIn: globals.config.jwtToken.expiration }, (token) => {
+    var token = jwt.sign({ id: params.id }, globals.config.jwtToken.secret, {
+      expiresIn: globals.config.jwtToken.expiration ,
+      subject: 'USER_AUTHENTICATION'
+    }, (token) => {
       callback(null, {
         token: token
       });
--- src/server/actions/authentications/verifyToken.js
+++ src/server/actions/authentications/verifyToken.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 
 export default function (globals) {
   return function verifyToken(token, callback) {
-    var token = jwt.verify(token, globals.config.jwtToken.secret, {}, (err, decoded) => {
+    var token = jwt.verify(token, globals.config.jwtToken.secret, {
+      subject: 'USER_AUTHENTICATION'
+    }, (err, decoded) => {
       if (err) {
         return callback(err);
       }
